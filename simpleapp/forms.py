b74102503c44d31128002199afdb702617d2ef2d
Jed <jedcrb@mail.ru>
2024-01-12 16:49:22 +1000

    D7.4. Валидация данных в формах

--- simpleapp/forms.py
+++ simpleapp/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [
+            'name',
+            'description',
+            'category',
+            'price',
+            'quantity',
+        ]
+
+    # дополнительные свои контроли полей формы
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get("name")
+        description = cleaned_data.get("description")
+
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентичным названию."
+            )
+
+        return cleaned_data
